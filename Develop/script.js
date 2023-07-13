14f769f4c36ea5677f6bf7b34236973aaed0fc31
Bwainuna <blakebah2@gmail.com>
2023-07-13 13:16:46 -0600

    added the code to generate passwords, updated README file, and added comments to JavaScript

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,60 @@
 // Assignment code here
 
+// Setting up the variables
+
+var lowercaseChars = "abcdefghijklmnopqrstuvwxyz";
+var uppercaseChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var numericChars = "0123456789";
+var specialChars = "!#$%&'()*+,-./:;<=>?@]^_`{|}~[";
+
+// Function to generate the password and rules for the password.
+
+function generatePassword() {
+  var passwordLength = prompt("Enter the length of new password (8-128 characters):");
+  passwordLength = parseInt(passwordLength);
+
+  // Prompt for characters to include in the password.
+
+  var includeNumeric = confirm("Include numeric characters?");
+  var includeUppercase = confirm("Include uppercase characters?");
+  var includeLowercase = confirm("Include lowercase characters?");
+  var includeSpecial = confirm("Include special characters?");
+
+  // Validates that at least one character type is selected
+  if (!includeLowercase && !includeUppercase && !includeNumeric && !includeSpecial) {
+    alert("Select at least one character type.");
+    return "";
+  }
+
+// If statements to determine what characters to add to password if chosen to include these variables.
+
+var selectedChar = "";
+  if (includeLowercase) {
+    selectedChar += lowercaseChars;
+  }
+  if (includeUppercase) {
+    selectedChar += uppercaseChars;
+  }
+  if (includeNumeric) {
+    selectedChar += numericChars;
+  }
+  if (includeSpecial) {
+    selectedChar += specialChars;
+  }
+
+// creates the password and stops it from generating past the selected number of characters.
+
+var password = "";
+  for (var i = 0; i < passwordLength; i++) {
+    var randomIndex = Math.floor(Math.random() * selectedChar.length);
+    password += selectedChar.charAt(randomIndex);
+  }
+
+  return password;
+}
+
+var passwordText = document.querySelector("#password");
+
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
--- README.md
+++ README.md
@@ -1,1 +1,39 @@
 # 03-Blake-Boiteux-Password-Generator
+Module 3 Challenge - Password Generator.
+
+User Story
+
+AS AN employee with access to sensitive data
+I WANT to randomly generate a password that meets certain criteria
+SO THAT I can create a strong password that provides greater security
+
+Acceptance Criteria
+
+GIVEN I need a new, secure password
+WHEN I click the button to generate a password
+THEN I am presented with a series of prompts for password criteria
+WHEN prompted for password criteria
+THEN I select which criteria to include in the password
+WHEN prompted for the length of the password
+THEN I choose a length of at least 8 characters and no more than 128 characters
+WHEN asked for character types to include in the password
+THEN I confirm whether or not to include lowercase, uppercase, numeric, and/or special characters
+WHEN I answer each prompt
+THEN my input should be validated and at least one character type should be selected
+WHEN all prompts are answered
+THEN a password is generated that matches the selected criteria
+WHEN the password is generated
+THEN the password is either displayed in an alert or written to the page
+
+
+screenshots of the deployed application:
+
+
+
+Sources that were used during this assignment came from class in canvas, as well as through the website listed below:
+
+https://www.w3schools.com/
+https://dev.to/code_mystery/random-password-generator-using-javascript-6a
+https://openai.com/blog/chatgpt
+
+Starter code for this assignment was provided through class at: https://github.com/coding-boot-camp/friendly-parakeet by Xandromus
